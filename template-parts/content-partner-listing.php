e860088b05ec6b54f41991061df9cad789009b8b
Dave White <david@dvdwhite.com>
2017-08-24 06:40:25 -0600

    Partner list

--- template-parts/content-partner-listing.php
+++ template-parts/content-partner-listing.php
@@ -17,22 +17,35 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<table>
-		<?php
-			the_content();
+		<div class="row">
+			<div class="col-sm-6">
+				<table class="partner-table">
+				<?php
+					the_content();
 
-			$partners = get_users( 'blog_id=1&orderby=nicename&role=partner' );
-			$partner_contacts = array();  
+					$partners = get_users( 'blog_id=1&orderby=nicename&role=partner' );
+					$partner_contacts = array();  
 
-		    foreach ( $partners as $partner ) {
-		    	$partner_contacts[$partner->user_nicename] = $partner->user_firstname . ' ' . $partner->user_lastname ;
-		    	echo '<tr><td>' . $partner->user_firstname . ' ' . $partner->user_lastname . '</td></tr>';
-		    }    
-    
-			//var_dump($partner_contacts);
+				    foreach ( $partners as $partner ) {
+				    	$partner_contacts[$partner->user_nicename] = $partner->user_firstname . ' ' . $partner->user_lastname ;
+				    	echo '<tr>' .
+				    		 '<td>' . $partner->user_firstname . ' ' . $partner->user_lastname . '</td>' .
+				    		 '<td>NEED STATE</td>' . 
+				    		 '<td>NEED REGION</td>' .
+				    		 '</tr>';
 
-		?>
-	</table>
+				    	// NEED TO GET STATE USER IS ASSIGNED TO
+
+				    	// NEED TO GET REGION STATE IS ASSOCIATED WITH
+
+				    }    
+		    
+					//var_dump($partner_contacts);
+
+				?>
+				</table>
+			</div>
+		</div>
 	</div><!-- .entry-content -->
 </div>
 
--- template-parts/content-state.php
+++ template-parts/content-state.php
@@ -27,7 +27,7 @@
 			<div class="state-meta row">
 				<div class="col-sm-9">
 
-					<table cellpadding="0" cellspacing="0" border="0">
+					<table cellpadding="0" cellspacing="0" border="0" class="partner-table">
 						<tr>
 							<td>AMBER Alert Region</td>
 							<td><?php echo rwmb_meta('amber_region');?></td>
