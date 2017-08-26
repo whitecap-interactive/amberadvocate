a933dd7b8c9483e294596178772d651ed00d2636
bronnt <bthurber@Brian-T.local>
2017-08-26 08:45:05 -0600

    user id, state, state id

--- template-parts/content-partner-listing.php
+++ template-parts/content-partner-listing.php
@@ -24,21 +24,21 @@
 					the_content();
 
 					$partners = get_users( 'blog_id=1&orderby=nicename&role=partner' );
-					$partner_contacts = array();  
+					$partner_contacts = array(); 
 				    foreach ( $partners as $partner ) {
-				    	$partner_contacts[$partner->user_nicename] = $partner->user_firstname . ' ' . $partner->user_lastname ;
-				    	$partner_state = get_user_meta($partner->ID, 'your_state', true);
-				    	foreach ($partner_state as $state_id) {
-				    		if ( $post = get_page_by_path( $state_id, OBJECT, 'state' ) )
-								    $id = $post->ID;
-								else
-								    $id = 0;
-				    	}
+				    	/*$partner_contacts[$partner->user_nicename] = $partner->user_firstname . ' ' . $partner->user_lastname ;*/
+				    	$partner_state_name = get_user_meta($partner->ID, 'your_state', true);
+			    		if ( $post = get_page_by_path( $partner_state_name, OBJECT, 'state' ) ){
+			    			$id = $post->ID;
+			    		}    
+						else{
+							$id = 0;
+						}	  
 				    	echo '<tr>' .
 				    		 '<td>' . $partner->user_firstname . ' ' . $partner->user_lastname . '</td>' .
 				    		 '<td>Partner ID: ' . $partner->ID . '</td>' .
-				    		 '<td>' . $partner_state . '</td>' . 
-				    		 '<td>' . get_user_meta($partner->ID, 'your_state', true) . '</td>' .
+				    		 '<td>State Name: ' . $partner_state_name . '</td>' . 
+				    		 '<td>State ID: ' . $id/*get_user_meta($partner->ID, 'your_state', true)*/ . '</td>' .
 				    		 '</tr>';
 
 				    	// NEED TO GET REGION STATE IS ASSOCIATED WITH
