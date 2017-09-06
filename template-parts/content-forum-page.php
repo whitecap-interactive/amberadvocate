4f5682cd155e70379278b7c7cc5a76f1b06f2bf1
bronnt <bthurber@Brian-T.local>
2017-09-06 09:32:00 -0600

    forum custom template files

---
+++ template-parts/content-forum-page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package amberadvocate
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php if ( is_user_logged_in() ) { 
+		     the_content();
+			} 
+			else { 
+			    echo 'You must be logged in to view the partner discussion board. To log in, please visit the <a href="/partner-portal/" title="Partner Portal Home Page" rel="home">Partner Portal Home Page</a>';
+			}
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'amberadvocate' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
