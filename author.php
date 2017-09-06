db05918a1d8beebf178b4e32568ca50b24e95168
Dave White <david@dvdwhite.com>
2017-09-06 09:20:16 -0600

    partner listing default view

--- author.php
+++ author.php
@@ -16,67 +16,6 @@ get_header(); ?>
                 <div class="row state-meta">
                     <div class="col-sm-6">
 
-
-                        <div id="content" class="narrowcolumn">
-
-                            <!-- This sets the $curauth variable -->
-
-                            <?php
-                                $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
-                                //var_dump ( $curauth );
-                                $partner = $curauth->id;
-                                $partner_state = get_user_meta($partner, 'state', true);
-                                $partner_role =  get_user_meta($partner, 'role', true);
-                                $partner_bio =  get_user_meta($partner, 'description', true);
-                                $partner_office_phone =  get_user_meta($partner, 'office_phone', true);
-                                $partner_mobile_phone =  get_user_meta($partner, 'cellular_or_alternate_number', true);
-                                $partner_rank_or_title =  get_user_meta($partner, 'rank_or_title', true);
-                                $partner_employing_agency =  get_user_meta($partner, 'employing_agency', true);
-                            ?>
-
-                            <table cellpadding="0" cellspacing="0" border="0" class="partner-table">
-                                <tr>
-                                    <th colspan="2"><strong><?php echo $curauth->first_name . ' ' . $curauth->last_name; ?></strong></th>
-                                </tr>
-                                <tr>
-                                    <td>State</td>
-                                    <td><?php echo $partner_state; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Role</td>
-                                    <td><?php echo $partner_role; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Bio</td>
-                                    <td><?php echo wpautop( get_the_author_meta( 'description' ) ); ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Office Phone</td>
-                                    <td><?php echo $partner_office_phone; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Mobile/Secondary Phone</td>
-                                    <td><?php echo $partner_mobile_phone; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Email</td>
-                                    <td><?php echo $curauth->user_email; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Rank or Title</td>
-                                    <td><?php echo $partner_rank_or_title; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Employing Agency</td>
-                                    <td><?php echo $partner_employing_agency; ?></td>
-                                </tr>
-                            </table>
-
-
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-
                         <table class="partner-table">
                         <tr>
                             <th><strong>Name</strong></th>
@@ -104,6 +43,62 @@ get_header(); ?>
                             }  
                         ?>
                         </table>
+
+                    </div>
+                    <div class="col-sm-6">
+
+                        <?php
+                            $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
+                            //var_dump ( $curauth );
+                            $partner = $curauth->id;
+                            $partner_state = get_user_meta($partner, 'state', true);
+                            $partner_role =  get_user_meta($partner, 'role', true);
+                            $partner_bio =  get_user_meta($partner, 'description', true);
+                            $partner_office_phone =  get_user_meta($partner, 'office_phone', true);
+                            $partner_mobile_phone =  get_user_meta($partner, 'cellular_or_alternate_number', true);
+                            $partner_rank_or_title =  get_user_meta($partner, 'rank_or_title', true);
+                            $partner_employing_agency =  get_user_meta($partner, 'employing_agency', true);
+                        ?>
+
+                        <table cellpadding="0" cellspacing="0" border="0" class="partner-table">
+                            <tr>
+                                <th colspan="2"><strong><?php echo $curauth->first_name . ' ' . $curauth->last_name; ?></strong></th>
+                            </tr>
+                            <tr>
+                                <td>State</td>
+                                <td><?php echo $partner_state; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Role</td>
+                                <td><?php echo $partner_role; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Bio</td>
+                                <td><?php echo wpautop( get_the_author_meta( 'description' ) ); ?></td>
+                            </tr>
+                            <tr>
+                                <td>Office Phone</td>
+                                <td><?php echo $partner_office_phone; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Mobile/Secondary Phone</td>
+                                <td><?php echo $partner_mobile_phone; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td><?php echo $curauth->user_email; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Rank or Title</td>
+                                <td><?php echo $partner_rank_or_title; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Employing Agency</td>
+                                <td><?php echo $partner_employing_agency; ?></td>
+                            </tr>
+                        </table>
+
+
 
                     </div>
 
--- template-parts/content-partner-listing.php
+++ template-parts/content-partner-listing.php
@@ -19,7 +19,7 @@
 		<div class="row">
 			<?php if ( is_user_logged_in() ) { ?>
 
-				<div class="col-sm-12">
+				<div class="col-sm-6">
 					<table class="partner-table">
 						<tr>
 							<th><strong>Name</strong></th>
@@ -47,9 +47,62 @@
 					?>
 					</table>
 				</div>
-				<!-- <div class="col-sm-6">
+				<div class="col-sm-6">
 
-				</div> -->
+			        <?php
+			        	$user = wp_get_current_user();
+						//echo $user->id;
+                        $partner = $user->id;
+                        $partner_state = get_user_meta($partner, 'state', true);
+                        $partner_role =  get_user_meta($partner, 'role', true);
+                        $partner_bio =  get_user_meta($partner, 'description', true);
+                        $partner_office_phone =  get_user_meta($partner, 'office_phone', true);
+                        $partner_mobile_phone =  get_user_meta($partner, 'cellular_or_alternate_number', true);
+                        $partner_rank_or_title =  get_user_meta($partner, 'rank_or_title', true);
+                        $partner_employing_agency =  get_user_meta($partner, 'employing_agency', true);
+                    ?>
+
+                    <table cellpadding="0" cellspacing="0" border="0" class="partner-table">
+                        <tr>
+                            <th colspan="2"><strong>My Profile: <?php echo $user->first_name . ' ' . $user->last_name; ?></strong></th>
+                        </tr>
+                        <tr>
+                            <td>State</td>
+                            <td><?php echo $partner_state; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Role</td>
+                            <td><?php echo $partner_role; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Bio</td>
+                            <td><?php echo wpautop( get_the_author_meta( 'description' ) ); ?></td>
+                        </tr>
+                        <tr>
+                            <td>Office Phone</td>
+                            <td><?php echo $partner_office_phone; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Mobile/Secondary Phone</td>
+                            <td><?php echo $partner_mobile_phone; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Email</td>
+                            <td><?php echo $curauth->user_email; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Rank or Title</td>
+                            <td><?php echo $partner_rank_or_title; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Employing Agency</td>
+                            <td><?php echo $partner_employing_agency; ?></td>
+                        </tr>
+                    </table>
+
+                    <a class="button" href="/partner-portal/profile">Edit Profile</a>
+
+				</div>
 
 			<?php } else { 
 				get_sidebar('authenticate'); 
