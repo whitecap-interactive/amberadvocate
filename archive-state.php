92beacb99d3f9ce719abaaa2151bec3353657569
Dave White <david@dvdwhite.com>
2017-09-14 20:24:28 -0600

    added header to state pages

--- archive-state.php
+++ archive-state.php
@@ -16,43 +16,46 @@ get_header(); ?>
 		<?php
 		if ( have_posts() ) : ?>
 
-		<div class="content-channel channel-padding">
+		
 
 			<header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_title( '<h1 class="post-page-title">', '</h1>' );
 					the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
+            <div class="content-channel channel-padding">
 
-            <div class="row">
-                <?php echo do_shortcode('[usahtml5map id="0"]');?>
-            </div>
+                <div class="row">
+                    <?php echo do_shortcode('[usahtml5map id="0"]');?>
+                </div>
 
-			<table id="organization-table" class="organization-list">
-			    <?php
+    			<table id="organization-table" class="organization-list">
+    			    <?php
 
-                    // set up our archive arguments
-                    $archive_args = array(
-                        'post_type' => 'state',    // get only posts
-                        'posts_per_page'=> -1,   // this will display all posts on one page
-                        'orderby' => 'title',
-                        'order'   => 'ASC',
-                    );
+                        // set up our archive arguments
+                        $archive_args = array(
+                            'post_type' => 'state',    // get only posts
+                            'posts_per_page'=> -1,   // this will display all posts on one page
+                            'orderby' => 'title',
+                            'order'   => 'ASC',
+                        );
 
-                    // new instance of WP_Quert
-                    $archive_query = new WP_Query( $archive_args );
+                        // new instance of WP_Quert
+                        $archive_query = new WP_Query( $archive_args );
 
-                  ?>
+                      ?>
 
-                <?php /* Start the Loop */ ?>
-                <?php while ( $archive_query->have_posts() ) : $archive_query->the_post(); // run the custom loop ?>
+                    <?php /* Start the Loop */ ?>
+                    <?php while ( $archive_query->have_posts() ) : $archive_query->the_post(); // run the custom loop ?>
 
-                    <?php get_template_part( 'template-parts/content', 'state-list' ); ?>
+                        <?php get_template_part( 'template-parts/content', 'state-list' ); ?>
 
-                <?php endwhile; ?>
-                </table>
+                    <?php endwhile; ?>
+                    </table>
+
+                </div>
 
         <?php else : ?>
 
@@ -60,7 +63,6 @@ get_header(); ?>
 
         <?php endif; ?>
 
-    </div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- single-state.php
+++ single-state.php
@@ -11,17 +11,16 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<div class="content-channel channel-padding">
+			
 
-				<?php
-				while ( have_posts() ) : the_post();
+			<?php
+			while ( have_posts() ) : the_post();
 
-					get_template_part( 'template-parts/content', 'state' );
+				get_template_part( 'template-parts/content', 'state' );
 
-				endwhile; // End of the loop.
-				?>
-				
-			</div>
+			endwhile; // End of the loop.
+			?>
+			
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
