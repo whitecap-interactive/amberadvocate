d572a8aef39033ed023a94c25a378c1739c61d11
bronnt <bthurber@brian-t.campus.utah.edu>
2017-09-18 10:40:12 -0600

    Text change sidebar nav

--- sidebar-posts.php
+++ sidebar-posts.php
@@ -16,7 +16,7 @@
 	
 	<ul class="post-links">
 		<li class="blue"><i class="fa fa-lg fa-newspaper-o" aria-hidden="true"></i><a href="/category/amber-news">AMBER Alert in the News</a></li>
-		<li class="purple"><i class="fa fa-lg fa-smile-o" aria-hidden="true"></i><a href="/category/amber-speaks">Faces of the AMBER Alert</a></li>
+		<li class="purple"><i class="fa fa-lg fa-smile-o" aria-hidden="true"></i><a href="/category/amber-speaks">Faces of the AMBER Network</a></li>
 		<li class="red"><i class="fa fa-lg fa-road" aria-hidden="true"></i><a href="/category/on-the-front-lines">On the Front Lines</a></li>
 		<li class="yellow"><i class="fa fa-lg fa-globe" aria-hidden="true"></i><a href="/category/international">AMBER Alert International</a></li>
 		<li class="orange"><i class="fa fa-lg fa-map-marker" aria-hidden="true"></i><a href="/amber-alert-in-indian-country">AMBER Alert in Indian Country</a></li>
