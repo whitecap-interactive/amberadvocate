2157eb3b1fba4ae4726e7f4a56ca8f3c4ac4bd9a
Dave White <david@dvdwhite.com>
2018-05-21 14:55:57 -0600

    updated sidebar links

--- sidebar-posts.php
+++ sidebar-posts.php
@@ -15,12 +15,22 @@
 
 	
 	<ul class="post-links">
-		<li class="blue"><i class="fa fa-lg fa-newspaper-o" aria-hidden="true"></i><a href="/category/amber-news">AMBER Alert in the News</a></li>
+		<!-- <li class="blue"><i class="fa fa-lg fa-newspaper-o" aria-hidden="true"></i><a href="/category/amber-news">AMBER Alert in the News</a></li>
 		<li class="purple"><i class="fa fa-lg fa-smile-o" aria-hidden="true"></i><a href="/category/amber-speaks">Faces of the AMBER Network</a></li>
 		<li class="red"><i class="fa fa-lg fa-road" aria-hidden="true"></i><a href="/category/on-the-front-lines">On the Front Lines</a></li>
 		<li class="yellow"><i class="fa fa-lg fa-globe" aria-hidden="true"></i><a href="/category/international">AMBER Alert International</a></li>
 		<li class="orange"><i class="fa fa-lg fa-map-marker" aria-hidden="true"></i><a href="/amber-alert-in-indian-country">AMBER Alert in Indian Country</a></li>
-		<li class="gray"><i class="fa fa-lg fa-caret-square-o-down" aria-hidden="true"></i><a href="/category/briefs">AMBER Alert Briefs</a></li>
+		<li class="gray"><i class="fa fa-lg fa-caret-square-o-down" aria-hidden="true"></i><a href="/category/briefs">AMBER Alert Briefs</a></li> -->
+		<li class="dark-orange"><a href="/training-resources"><i class="fa fa-lg fa-university" aria-hidden="true"></i> Training & Events</a></li>
+		<li class="blue"><a href="/category/amber-news"><i class="fa fa-lg fa-newspaper-o" aria-hidden="true"></i> Featured AMBER News</a></li>
+		<li class="purple"><a href="/category/amber-speaks"><i class="fa fa-lg fa-smile-o" aria-hidden="true"></i> Faces of the AMBER Network</a></li>
+		<li class="red"><a href="/amber-alert-network/amber-alert-in-action"><i class="fa fa-lg fa-road" aria-hidden="true"></i> On the Front Lines</a></li>
+		<li class="yellow"><a href="/amber-alert-network/amber-alert-in-action"><i class="fa fa-lg fa-globe" aria-hidden="true"></i> AMBER Alert International</a></li>
+		<li class="orange"><a href="/amber-alert-in-indian-country/"><i class="fa fa-lg fa-map-marker" aria-hidden="true"></i> AMBER Alert in Indian Country</a></li>
+		<li class="gray"><a href="/category/briefs"><i class="fa fa-lg fa-caret-square-o-down" aria-hidden="true"></i> AMBER Alert Briefs</a></li>
+		<li class="green"><a href="/partner-portal"><i class="fa fa-lg fa-users" aria-hidden="true"></i> Partner Portal</a></li>
+		<li class="dark-orange"><a href="/category/partner-events"><i class="fa fa-lg fa-calendar" aria-hidden="true"></i> Partner Events</a></li>
+		<li class="blue"><a href="/category/partner-news"><i class="fa fa-lg fa-newspaper-o" aria-hidden="true"></i> Partner News</a></li>		
 	</ul>
 
 
