ba1fcd2773a55a8c856f2e9787f1385e4f76eae3
Dave White <david@dvdwhite.com>
2019-04-02 21:36:47 -0600

    CART DB: template files

---
+++ template-parts/content-cart-template.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package amberadvocate
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+    <header class="post-entry-header purple">
+        <?php
+            $subhead = rwmb_meta('amber_subhead');
+            if (!empty($subhead)) { 
+                the_title( '<h1 class="entry-title"><span>', '</span></h1>' ); 
+                echo '<h3>' . rwmb_meta('amber_subhead') . '</h3>'; 
+            } else { the_title( '<h1 class="entry-title">', '</h1>' ); }
+        ?>
+    </header><!-- .entry-header -->
+
+	<div class="content-channel channel-padding">
+		<div class="row">
+			
+
+			<div class="col-sm-9">
+				<?php the_content(); ?>
+			</div>
+			<div class="col-sm-3">
+				<?php get_sidebar('portal'); ?>
+			</div>
+				
+		</div>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
