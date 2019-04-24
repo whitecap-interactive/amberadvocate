a1dcf333d0ae035eb04c33b69bc78c189417f885
dvdwhite <david@dvdwhite.com>
2019-04-24 11:23:42 -0600

    CART DB: portal links fix

--- sidebar-cart.php
+++ sidebar-cart.php
@@ -9,14 +9,20 @@
 
 ?>
 
-<div class="partner-links-container">
+<?php
+	$user = wp_get_current_user();
+	if ( in_array( 'cart_admin', (array) $user->roles ) ) { ?>
 
-	<h3>CART Database Links</h3>
+		<div class="partner-links-container">
 
-	<ul class="partner-links">
-		<li><i class="fa fa-address-book" aria-hidden="true"></i><a href="/cart-admin/cart-listing">CART Listing</a></li>
-		<li><i class="fa fa-file-o" aria-hidden="true"></i><a href="/cart-admin/add-edit-cart">Add a CART</a></li>
-	</ul>
+			<h3>CART Database Links</h3>
+
+			<ul class="partner-links">
+				<li><i class="fa fa-address-book" aria-hidden="true"></i><a href="/cart-admin/cart-listing">CART Listing</a></li>
+				<li><i class="fa fa-file-o" aria-hidden="true"></i><a href="/cart-admin/add-edit-cart">Add a CART</a></li>
+			</ul>
 
 
-</div>
+		</div>
+
+<?php } ?>
