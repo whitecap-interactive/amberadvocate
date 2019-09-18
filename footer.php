a88f24b5fc19f3997e468745c46d9093036e966f
dvdwhite <david@dvdwhite.com>
2019-09-17 20:51:41 -0600

    Footer (not complete)

--- footer.php
+++ footer.php
@@ -15,44 +15,36 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<div class="footer-links">
+			<div class="footer-content-container">
 				<div class="content-channel row">
 					<div class="col-sm-8">
-						<nav id="bottom-nav" role="navigation">
-							<?php wp_nav_menu( array(
-								'theme_location' 	=> 'footer',
-								'menu_class' 		=> 'bottom-menu menu-trigger',
-								'container' 		=> false,
-								'before'      => '<h3>',
-								'after'       => '</h3>',
-								'items_wrap'    => '<ul id="%1$s" class="%2$s" rel="bottom">%3$s</ul>'
-							) ); ?>
-						</nav>
+						<div class="footer-logos">
+							<div class="footer-logo-justice"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-justice.png" width="100" border="0" alt="Department of Justice - Office of Justice Programs" /></div>
+							<div class="footer-logo-amber"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-amber.png" width="130" border="0" alt="AMBER Alert" /></div>
+							<div class="footer-address">
+								Address goes here.<br />
+								555 Turbo Way<br />
+								SLC, UT 84121<br /><br />
+								<a href="mailto:info@fvtc.edu">info@fvtc.edu</a><br />
+								(801) 555-5555
+							</div>
+						</div>
+						<div class="footer-copyright">
+							<!-- &copy; --> <?php echo date("Y");?> AMBER Alert Training and Technical Assistance Program
+							<?php
+								if (is_front_page()){
+									echo '<p><a href="/terms-of-use-and-privacy-policy/">Terms of Use and Privacy Policy</a></p>';
+								}
+							?>
+						</div>
 					</div>
-					<div class="col-sm-4 footer-login">
-						<h3>Partner Portal Login</h3>
-						<?php echo do_shortcode('[user-meta-login]');?>
-						<?php
-							$user = wp_get_current_user();
-							if ( in_array( 'cart_admin', (array) $user->roles ) ) {
-								echo '<a class="button" style="margin-top: 4px; display: inline-block; padding: 2px 20px;" href="/cart-admin/cart-listing/">CART Admin Listing</a>';
-							}
-						?>
+					<div class="col-sm-4 footer-right-channel">
+						<div class="footer-social">
+							Social Links
+						</div>
+						<div class="footer-logo-advocate"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-advocate.png" width="154" height="21" border="0" alt="AMBER Advocate" /></div>
 					</div>
 				</div>
-			</div>
-			<div class="footer-logos">
-				<div class="footer-logo-justice"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-justice.png" width="73" height="73" border="0" alt="Department of Justice - Office of Justice Programs" /></div>
-				<div class="footer-logo-amber"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-amber.png" width="100" height="60" border="0" alt="AMBER Alert" /></div>
-				<div class="footer-logo-advocate"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-advocate.png" width="154" height="21" border="0" alt="AMBER Advocate" /></div>
-			</div>
-			<div class="footer-copyright">
-				<!-- &copy; --> <?php echo date("Y");?> AMBER Alert Training and Technical Assistance Program
-				<?php
-					if (is_front_page()){
-						echo '<p><a href="/terms-of-use-and-privacy-policy/">Terms of Use and Privacy Policy</a></p>';
-					}
-				?>
 			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
--- sidebar-questions.php
+++ sidebar-questions.php
@@ -1,30 +1,43 @@
 
 <div class="question-sidebar-container">
 
-	<h1 class='question-headline'><span>Questions?</span></h1>	
-	<div class="question-line"></div>				
-	<h3 class='question-sub-headline'>For questions about using the site or your existing membership, please use the 'Contact Us' button. </h3>
+	<div class="content-channel row">
+		<div class="col-sm-6">
+			<h1 class='question-headline'><span>Questions?</span></h1>
+			<div class="question-line"></div>
+			<h3 class='question-sub-headline'>For questions about using the site or your existing membership, please use the 'Contact Us' button. </h3>
 
-	<div class="question-button">
-		<a href="/partner-portal/contact-us" class="question-button button-lg light-orange" >
-			<span>
-				Contact Us		
-			</span>
-		</a>
+			<div class="question-button">
+				<a href="/partner-portal/contact-us" class="question-button button-lg light-orange" >
+					<span>
+						Contact Us
+					</span>
+				</a>
 
 
-		<!-- <a href="/partner-portal/request-access" class="question-button button-lg light-orange" >
-			<span>
-				Request Access	
-			</span>
-		</a> -->
+				<!-- <a href="/partner-portal/request-access" class="question-button button-lg light-orange" >
+					<span>
+						Request Access
+					</span>
+				</a> -->
+			</div>
+		</div>
+		<div class="col-sm-6 footer-login">
+			<h1>Partner Portal Login</h1>
+			<?php echo do_shortcode('[user-meta-login]');?>
+			<?php
+				$user = wp_get_current_user();
+				if ( in_array( 'cart_admin', (array) $user->roles ) ) {
+					echo '<a class="button" style="margin-top: 4px; display: inline-block; padding: 2px 20px;" href="/cart-admin/cart-listing/">CART Admin Listing</a>';
+				}
+			?>
+		</div>
 	</div>
 
+
+
 	<div class="question-button">
 
-	</div>	
+	</div>
 
 </div>
-
-
-									
