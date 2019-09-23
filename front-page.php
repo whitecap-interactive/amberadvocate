05972c8975964c983d24079546ea3df0623532df
David White <david@dvdwhite.com>
2019-09-23 16:11:47 -0600

    homepage code snippet for Niki

--- front-page.php
+++ front-page.php
@@ -12,19 +12,22 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<div class="content-channel channel-padding">
-
-<!-- 				<div class="row">
+<!--
+ 				<div class="row">
 					<div class="col-sm-4">
 
 						<ul class="home-page-menu">
-						  <li class="green"><a href="/partner-portal"><i class="fa fa-lg fa-users" aria-hidden="true"></i> Partner Portal</a></li>
-						  <li class="dark-orange"><a href="/training-and-events"><i class="fa fa-lg fa-university" aria-hidden="true"></i> Training & Events</a></li>
-						  <li class="blue"><a href="#"><i class="fa fa-lg fa-newspaper-o" aria-hidden="true"></i> Featured AMBER News</a></li>
-						  <li class="purple"><a href="#"><i class="fa fa-lg fa-smile-o" aria-hidden="true"></i> Faces of the AMBER Alert</a></li>
-						  <li class="red"><a href="#"><i class="fa fa-lg fa-road" aria-hidden="true"></i> On the Front Lines</a></li>
-						  <li class="yellow"><a href="#"><i class="fa fa-lg fa-globe" aria-hidden="true"></i> AMBER Alert International</a></li>
-						  <li class="orange"><a href="#"><i class="fa fa-lg fa-map-marker" aria-hidden="true"></i> AMBER Alert in Indian Country</a></li>
-						  <li class="gray"><a href="#"><i class="fa fa-lg fa-caret-square-o-down" aria-hidden="true"></i> AMBER Alert Briefs</a></li>
+						 <li class="dark-orange-border"><a href="/training-resources"> Training & Events</a></li>
+						 <li class="green-border"><a href="/category/upcoming-webinars"> Upcoming Webinars</a></li>
+						 <li class="blue-border"><a href="/category/amber-news"> Featured AMBER News</a></li>
+						 <li class="purple-border"><a href="/category/amber-speaks"> Faces of the AMBER Network</a></li>
+						 <li class="red-border"><a href="/amber-alert-network/amber-alert-in-action"> On the Front Lines</a></li>
+						 <li class="yellow-border"><a href="/amber-alert-network/amber-alert-in-action"> AMBER Alert International</a></li>
+						 <li class="orange-border"><a href="/amber-alert-in-indian-country/"> AMBER Alert in Indian Country</a></li>
+						 <li class="gray-border"><a href="/category/briefs"> AMBER Alert Briefs</a></li>
+						 <li class="green-border"><a href="/partner-portal"> Partner Portal</a></li>
+						 <li class="dark-orange-border"><a href="/category/partner-events"> Partner Events</a></li>
+						 <li class="blue-border"><a href="/category/partner-news"> Partner News</a></li>
 						</ul>
 
 					</div>
