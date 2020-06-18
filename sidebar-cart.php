cca6aedbb1ff382d02bc334b81af18dc156d014e
dvdwhite <dave.orrock.white@gmail.com>
2020-06-18 12:03:50 -0600

    CART DB admin links

--- sidebar-cart.php
+++ sidebar-cart.php
@@ -20,6 +20,10 @@
 			<ul class="partner-links">
 				<li><i class="fa fa-address-book" aria-hidden="true"></i><a href="/cart-admin/cart-listing">CART Listing</a></li>
 				<li><i class="fa fa-file-o" aria-hidden="true"></i><a href="/cart-admin/add-edit-cart">Add a CART</a></li>
+				<li><i class="fa fa-file-o" aria-hidden="true"></i><a href="https://www.amberadvocate.org/wp-content/uploads/2020/06/CART_Assessor_Field_Checklist_fillable.pdf" target="_blank">Field Exercise – Assessor’s Detailed Checklist</a></li>
+				<li><i class="fa fa-file-o" aria-hidden="true"></i><a href="https://www.amberadvocate.org/wp-content/uploads/2020/06/CART_Field_Exercise_Assessor_SUMMARY_FINDINGS_fillable.pdf" target="_blank">Field Exercise – Assessor’s SUMMARY Findings Report</a></li>
+				<li><i class="fa fa-file-o" aria-hidden="true"></i><a href="https://www.amberadvocate.org/wp-content/uploads/2018/03/CART-Standard-Compliance-Document-fillable.pdf" target="_blank">CART Standard Compliance Document</a></li>
+				<li><i class="fa fa-file-o" aria-hidden="true"></i><a href="https://www.amberadvocate.org/wp-content/uploads/2020/06/CART-Member-Interview-Form-fillable-1.pdf" target="_blank">CART Member Interview Form</a></li>
 			</ul>
 
 
